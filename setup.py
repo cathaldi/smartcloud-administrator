ac9229035fb759b89d8527378071da12de05f1e5
cathaldi <cathaldi@ie.ibm.com>
2019-06-04 11:57:32 +0100

    converted folder to package

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='smartcloudadmin',
-    version='0.7.2',
+    version='0.7.3',
     packages=['smartcloudadmin', 'smartcloudadmin.utils', 'smartcloudadmin.models', 'smartcloudadmin.json'],
     url='https://github.com/cathaldi/smartcloud-administrator',
     include_package_data=True,
     license='apache2',
-    download_url='https://github.com/cathaldi/smartcloud-administrator/releases/download/0.7.2/smartcloudadmin-0.7.2.tar.gz',
+    download_url='https://github.com/cathaldi/smartcloud-administrator/releases/download/0.7.2/smartcloudadmin-0.7.3.tar.gz',
     author='Cathal A. Dinneen',
     install_requires=['requests'],
     author_email='cathal.a.dinneen@gmail.com',
