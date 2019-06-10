1c724bb767522d3970d16d88f4cfb39de0bb22af
cathaldi <cathaldi@ie.ibm.com>
2019-06-10 16:24:29 +0100

    added manifest back

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='smartcloudadmin',
-    version='0.7.4',
+    version='0.7.5',
     packages=['smartcloudadmin', 'smartcloudadmin.utils', 'smartcloudadmin.models', 'smartcloudadmin.json'],
     url='https://github.com/cathaldi/smartcloud-administrator',
     include_package_data=True,
+    package_data={'': ['*']},
     license='apache2',
-    download_url='https://github.com/cathaldi/smartcloud-administrator/releases/download/0.7.4/smartcloudadmin-0.7.4.tar.gz',
+    download_url='https://github.com/cathaldi/smartcloud-administrator/releases/download/0.7.5/smartcloudadmin-0.7.5.tar.gz',
     author='Cathal A. Dinneen',
     install_requires=['requests'],
     author_email='cathal.a.dinneen@gmail.com',
